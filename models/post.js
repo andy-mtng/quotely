746dbfea8c80d11612644f8cb3c3836c92d91a88
Andy N <andynguyen9001@gmail.com>
2022-09-29 20:56:13 -0600

    Add date-fns post formatting

--- models/post.js
+++ models/post.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { format } = require('date-fns');
 
 const PostSchema = Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
     timeStamp: {type: Date, required: true},
     author: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true}
+});
+
+PostSchema.virtual('formattedDate').get(function() {
+    return format(this.timeStamp, 'MMM. d | p');
+    // format(new Date(2017, 10, 6), 'MMM')
 });
 
 module.exports = {
